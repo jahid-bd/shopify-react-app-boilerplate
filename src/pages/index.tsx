a7c3e19a00c8b378ff67fd436808d0c0efa22a1e
jahid-bd <jahid.hasan@jouleslabs.com>
2024-06-26 12:26:20 +0600

    feat: auth token log added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,25 @@
-import { TitleBar } from '@shopify/app-bridge-react';
+import { TitleBar, useAppBridge } from '@shopify/app-bridge-react';
 import { Page } from '@shopify/polaris';
 
 import DashboardView from '@/components/view/DashboardView';
+import { getSessionToken } from '@shopify/app-bridge-utils';
+import { useEffect } from 'react';
 import '../styles/globals.css';
 
 export default function HomePage() {
+  // check authentication when app run in the admin
+  const app = useAppBridge();
+  console.log('App', app);
+
+  const getToken = async () => {
+    const sessionToken = await getSessionToken(app);
+    console.log('sessionToken', sessionToken);
+  };
+
+  useEffect(() => {
+    getToken();
+  });
+
   return (
     <Page>
       <TitleBar title="Dashboard" />
